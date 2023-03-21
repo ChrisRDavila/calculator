8f341102390fc8e8bf667d54f388ca28d94dda89
ChrisRDavila <crossdavila@gmail.com>
2023-03-21 08:36:44 -0700

    debugged issues with numbers on alert

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 function divide(number1, number2) {
-  return number1 / number2
+  return number1 / number2  
 }
+
 function multiply(number1, number2) {
   return number1 * number2
 }
@@ -12,20 +13,21 @@ function subtract(number1, number2) {
 }
 //user interface logic
 const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter another number"));
+const number2 = parseInt(prompt("Enter another number:"));
 
 answer1 = (divide(number1, number2));
- 
-window.alert ("dividing them give us " + answer1 + ".");
+
+window.alert (number1 + "/" + number2 + "=" + answer1)
 
 answer2 = (multiply(number1, number2));
 
-window.alert ("when we multply them the answer is " + answer2 + ".");
+window.alert (number1 + "*" + number2 + "=" + answer2);
 
 answer3 = (add(number1, number2));
 
-window.alert ("when added we get " + answer3 + ".");
+window.alert (number1 + "+" + number2 + "=" + answer3);
 
 answer4 = (subtract(number1, number2));
 
-window.alert ("subrtacting them results in " + answer4)
+window.alert (number1 + "-" + number2 + "=" + answer4)
+
